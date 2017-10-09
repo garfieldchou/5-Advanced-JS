f47b1f1786ca119623a6717d19376a48d5c060e5
shaudai <shaudai0402@hotmail.com>
2017-10-09 10:04:48 +0800

    move the method to prototype property of Person for inheritance

--- script.js
+++ script.js
@@ -10,15 +10,16 @@ var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge = function() {
-        console.log(2016 - this.yearOfBirth);
-    }
+};
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
 };
 
 var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
 
 john.calculateAge();
-
-var jane = new Person('Jane', 1969, 'designer');
-
-var mark = new Person('Mark', 1948, 'retired');
+jane.calculateAge();
+mark.calculateAge();
