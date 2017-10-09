712caffb446b381ce80ab10d3c1fd291c638e53c
shaudai <shaudai0402@hotmail.com>
2017-10-09 11:02:18 +0800

    create an instance with Object.create way. Use an object as a prototype for inheritance

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Function constructor
-
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -29,3 +29,30 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+
+// Object.create
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
