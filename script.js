30ac0ef3ce0f7a0e789086c05e8f345975d26076
shaudai <shaudai0402@hotmail.com>
2017-10-09 10:10:46 +0800

    add lastName property to Person's prototype

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ Person.prototype.calculateAge = function() {
     console.log(2016 - this.yearOfBirth);
 };
 
+Person.prototype.lastName = 'Smith';
+
 var john = new Person('John', 1990, 'teacher');
 var jane = new Person('Jane', 1969, 'designer');
 var mark = new Person('Mark', 1948, 'retired');
@@ -23,3 +25,7 @@ var mark = new Person('Mark', 1948, 'retired');
 john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
