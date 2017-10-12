65a91ea82ddb66f7f99b00338cf040ae99eba969
shaudai <shaudai0402@hotmail.com>
2017-10-12 22:16:23 +0800

    passing functions as arguments

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ console.log(mark.lastName);
 */
 
 // Object.create
+/*
 var personProto = {
     calculateAge: function() {
         console.log(2016 - this.yearOfBirth);
@@ -48,3 +49,24 @@ var jane = Object.create(personProto, {
     yearOfBirth: { value: 1969 },
     job: { value: 'designer' }
 });
+*/
+
+//////////////////////////////////////////
+// Lecture: Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+var ages = arrayCalc(years, calculateAge);
+console.log(ages);
