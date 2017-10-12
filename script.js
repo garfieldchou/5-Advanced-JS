d86d2be1d6b2c9c45a73a55ff6c2b918032614d1
shaudai <shaudai0402@hotmail.com>
2017-10-13 07:46:02 +0800

    add parameter to a function

--- script.js
+++ script.js
@@ -131,5 +131,9 @@ game();
     console.log(score >=  5);
 })();
 
-console.log(score);
+// console.log(score);
 
+(function(goodLuck){
+    var score = Math.random() * 10;
+    console.log(score >=  5 - goodLuck);
+})(5);
