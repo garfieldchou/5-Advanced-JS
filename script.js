849b42615615d0590bde3b96cb73c33f57885722
shaudai <shaudai0402@hotmail.com>
2017-10-12 22:42:38 +0800

    return different function expressions to a variable

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ var jane = Object.create(personProto, {
 
 //////////////////////////////////////////
 // Lecture: Passing functions as arguments
-
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
@@ -87,3 +87,26 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+*/
+
+//////////////////////////////////////////
+// Lecture: Functions returning functions
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+teacherQuestion('John');
