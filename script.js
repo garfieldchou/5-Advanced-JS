c05f97c495a8b4f0fd508cdf19467dd626edf1d8
shaudai <shaudai0402@hotmail.com>
2017-10-13 11:35:22 +0800

    revise the interview function with the closure way

--- script.js
+++ script.js
@@ -160,12 +160,37 @@ retirementIceland(1990);
 
 // retirement(66)(1990);
 
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+*/
 
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
 
-
-
-
-
+interviewQuestion('teacher')('John');
 
 
 
