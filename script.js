72e7838f565dc7efd1896a44a111df3c620071be
shaudai <shaudai0402@hotmail.com>
2017-10-17 07:11:49 +0800

    suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code

--- script.js
+++ script.js
@@ -282,49 +282,51 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
-function Question(question, answers, correct) {
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-}
-
-Question.prototype.displayQuestion = function() {
-    console.log(this.question);
-    
-    for (var i = 0; i < this.answers.length; i++) {
-        console.log(i + ': ' +this.answers[i]);
+(function(){
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
     }
-}
 
-Question.prototype.checkAnswer = function(ans) {
-    if (ans === this.correct) {
-        console.log('Correct answer!');
-    } else {
-        console.log('Wrong answer. Try again :)');
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' +this.answers[i]);
+        }
     }
-}
 
-var q1 = new Question('Is JavaScript the coolest programming language in the world?',
-                     ['Yes', 'No'],
-                     0);
+    Question.prototype.checkAnswer = function(ans) {
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+        } else {
+            console.log('Wrong answer. Try again :)');
+        }
+    }
 
-var q2 = new Question('What is the name of this course\'s teacher?',
-                     ['John', 'Micheal', 'Jonas'],
-                     2);
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+                         ['Yes', 'No'],
+                         0);
 
-var q3 = new Question('What does best describe coding?',
-                     ['Boring', 'Hard', 'Fun', 'Tedious'],
-                     2);
+    var q2 = new Question('What is the name of this course\'s teacher?',
+                         ['John', 'Micheal', 'Jonas'],
+                         2);
 
-var questions = [q1, q2, q3];
+    var q3 = new Question('What does best describe coding?',
+                         ['Boring', 'Hard', 'Fun', 'Tedious'],
+                         2);
 
-var n = Math.floor(Math.random() * questions.length);
+    var questions = [q1, q2, q3];
 
-questions[n].displayQuestion();
+    var n = Math.floor(Math.random() * questions.length);
 
-var answer = parseInt(prompt('Please select the correct answer.'));
+    questions[n].displayQuestion();
 
-questions[n].checkAnswer(answer);
+    var answer = parseInt(prompt('Please select the correct answer.'));
+
+    questions[n].checkAnswer(answer);
+})();
 
 
 
