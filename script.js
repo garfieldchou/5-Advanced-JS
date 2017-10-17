b5f31686f3f85f46c3d61b1ae9554b29c9186990
shaudai <shaudai0402@hotmail.com>
2017-10-17 21:23:52 +0800

    after you display the result, display the next random question, so that the game never ends

--- script.js
+++ script.js
@@ -282,6 +282,7 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
+/*
 (function(){
     function Question(question, answers, correct) {
         this.question = question;
@@ -327,7 +328,7 @@ c) correct answer (I would use a number for this)
 
     questions[n].checkAnswer(answer);
 })();
-
+*/
 
 
 /*
@@ -342,7 +343,60 @@ c) correct answer (I would use a number for this)
 11. Display the score in the console. Use yet another method for this.
 */
 
+(function(){
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
 
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' +this.answers[i]);
+        }
+    }
+
+    Question.prototype.checkAnswer = function(ans) {
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+        } else {
+            console.log('Wrong answer. Try again :)');
+        }
+    }
+
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?',
+                         ['Yes', 'No'],
+                         0);
+
+    var q2 = new Question('What is the name of this course\'s teacher?',
+                         ['John', 'Micheal', 'Jonas'],
+                         2);
+
+    var q3 = new Question('What does best describe coding?',
+                         ['Boring', 'Hard', 'Fun', 'Tedious'],
+                         2);
+
+    var questions = [q1, q2, q3];
+    
+    function nextQuestion() {
+
+        var n = Math.floor(Math.random() * questions.length);
+
+        questions[n].displayQuestion();
+
+        var answer = parseInt(prompt('Please select the correct answer.'));
+
+        questions[n].checkAnswer(answer);
+        
+        nextQuestion();
+    
+    }
+    
+    nextQuestion();
+    
+})();
 
 
 
