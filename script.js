c5b3612c2b914485fbdeeca06d819b37d5e844fa
shaudai <shaudai0402@hotmail.com>
2017-10-17 23:24:00 +0800

    track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score

--- script.js
+++ script.js
@@ -358,12 +358,23 @@ c) correct answer (I would use a number for this)
         }
     }
 
-    Question.prototype.checkAnswer = function(ans) {
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
+        
         if (ans === this.correct) {
             console.log('Correct answer!');
+            sc = callback(true);
         } else {
             console.log('Wrong answer. Try again :)');
+            sc = callback(false);
         }
+        
+        this.displayScore(sc);
+    }
+    
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is: ' + score);
+        console.log('------------------------------');
     }
 
     var q1 = new Question('Is JavaScript the coolest programming language in the world?',
@@ -380,6 +391,17 @@ c) correct answer (I would use a number for this)
 
     var questions = [q1, q2, q3];
     
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }
+    }
+    var keepScore = score();
+    
     function nextQuestion() {
 
         var n = Math.floor(Math.random() * questions.length);
@@ -390,7 +412,7 @@ c) correct answer (I would use a number for this)
         
         if (answer !== 'exit') {
 
-            questions[n].checkAnswer(parseInt(answer));
+            questions[n].checkAnswer(parseInt(answer), keepScore);
 
             nextQuestion();
         
